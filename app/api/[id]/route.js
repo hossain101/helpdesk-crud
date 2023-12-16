971ed2da04a1948dd90c9df2c6ed0fe5e4a1efe3
shoaib <shoaibhossain24@gmail.com>
2023-12-16 20:08:55 +0600

    Refactor API route functions for error handeling

--- app/api/[id]/route.js
+++ app/api/[id]/route.js
@@ -2,27 +2,39 @@ import Ticket from "@/models/ticket";
 import { NextResponse } from "next/server";
 import connectMongoDB from "@/utils/mongodb";
 
-
-
 export const PUT = async (request, { params }) => {
-  const { id } = params;
-  const { newTitle: title, newDescription: description } = await request.json();
+  try {
+    const { id } = params;
+    const { newTitle: title, newDescription: description } = await request.json();
 
-  await connectMongoDB();
+    await connectMongoDB();
 
-  await Ticket.findByIdAndUpdate(id, { title, description });
+    const updatedTicket = await Ticket.findByIdAndUpdate(id, { title, description });
 
-  return NextResponse.json(
-    { message: "Ticket updated successfully!" },
-    { status: 200 }
-  );
-};  
+    if (!updatedTicket) {
+      return NextResponse.json({ message: "No ticket found with this ID" }, { status: 404 });
+    }
 
+    return NextResponse.json({ message: "Ticket updated successfully!" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "An error occurred while updating the ticket" }, { status: 500 });
+  }
+};
 
-export const GET = async (request,{params}) => {
+export const GET = async (request, { params }) => {
+  try {
     const { id } = params;
     await connectMongoDB();
     const ticket = await Ticket.findById(id);
-    return NextResponse.json(ticket);
-    
-    };
+
+    if (!ticket) {
+      return NextResponse.json({ message: "No ticket found with this ID" }, { status: 404 });
+    }
+
+    return NextResponse.json(ticket, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "An error occurred while fetching the ticket" }, { status: 500 });
+  }
+};
--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -3,42 +3,56 @@ import connectMongoDB from "@/utils/mongodb";
 import { NextResponse } from "next/server";
 
 export const POST = async (request) => {
-  const { title, description } = await request.json();
+  try {
+    const { title, description } = await request.json();
 
-  await connectMongoDB();
+    if (!title || !description) {
+      return NextResponse.json({ message: "Title and description are required" }, { status: 400 });
+    }
 
-  const ticket = await Ticket.create({
-    title,
-    description,
-  });
+    await connectMongoDB();
 
-  await ticket.save();
+    const ticket = await Ticket.create({ title, description });
 
-  return NextResponse.json(
-    { message: "Ticket created successfully!" },
-    { status: 201 }
-  );
+    return NextResponse.json({ message: "Ticket created successfully!", ticket }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "An error occurred while creating the ticket" }, { status: 500 });
+  }
 };
 
 export const GET = async () => {
-  await connectMongoDB();
+  try {
+    await connectMongoDB();
 
-  const tickets = await Ticket.find({});
+    const tickets = await Ticket.find({});
 
-  return NextResponse.json(tickets);
+    return NextResponse.json(tickets, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "An error occurred while fetching the tickets" }, { status: 500 });
+  }
 };
 
 export const DELETE = async (request) => {
-  const id = request.nextUrl.searchParams.get("id");
+  try {
+    const id = request.nextUrl.searchParams.get("id");
 
-  await connectMongoDB();
+    if (!id) {
+      return NextResponse.json({ message: "ID is required" }, { status: 400 });
+    }
 
-  await Ticket.findByIdAndDelete(id);
+    await connectMongoDB();
 
-  return NextResponse.json(
-    { message: "Ticket deleted successfully!" },
-    { status: 200 }
-  );
+    const deletedTicket = await Ticket.findByIdAndDelete(id);
+
+    if (!deletedTicket) {
+      return NextResponse.json({ message: "No ticket found with this ID" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Ticket deleted successfully!" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "An error occurred while deleting the ticket" }, { status: 500 });
+  }
 };
-
-
