f99ad31004537c0d0b46c444f7a05e4db2c869c8
shoaib <shoaibhossain24@gmail.com>
2023-12-17 02:32:07 +0600

    Refactor addTicket and TicketsList components

--- app/addTicket/page.jsx
+++ app/addTicket/page.jsx
@@ -1,15 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 const AddTicket = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        "http://localhost:3000/api/tickets" ||
+          "https://helpdesk-crud.onrender.com/api/tickets",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ title, description }),
+        }
+      );
+      const data = await res.json();
+      console.log(data);
+      if (res.status === 201) {
+        //rerender
+        router.replace(router.asPath);
+        router.push("/");
+      } else {
+        console.error("Error creating ticket!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className='flex flex-col items-center justify-center  min-h-screen bg-gradient-to-r from-green-400 to-blue-500 px-8'>
-      <form className='w-full max-w-lg bg-slate-600 rounded-lg shadow-md p-6'>
-        <div className='divide-y divide-gray-200'>
-          <input className='text-sm text-gray-400 rounded-lg w-full py-3 px-4 leading-tight focus:outline-none focus:shadow-outline mb-4' type="text" placeholder="Ticket Title" />
-          <input className='text-sm text-gray-400 rounded-lg w-full py-3 px-4 leading-tight focus:outline-none focus:shadow-outline mt-4 mb-6' type="text" placeholder="Ticket Description" />
-          <button className='w-full py-3 px-4 leading-none text-white bg-indigo-500 hover:bg-indigo-600 rounded-lg tracking-wide'>Add Ticket</button>
+    <div className="flex flex-col items-center justify-center  min-h-screen bg-gradient-to-r from-green-400 to-blue-500 px-8">
+      <form
+        className="w-full max-w-lg bg-slate-600 rounded-lg shadow-md p-6"
+        onSubmit={handleSubmit}
+      >
+        <div className="divide-y divide-gray-200">
+          <input
+            className="text-sm text-gray-400 rounded-lg w-full py-3 px-4 leading-tight focus:outline-none focus:shadow-outline mb-4"
+            type="text"
+            placeholder="Ticket Title"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+          <input
+            className="text-sm text-gray-400 rounded-lg w-full py-3 px-4 leading-tight focus:outline-none focus:shadow-outline mt-4 mb-6"
+            type="text"
+            placeholder="Ticket Description"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          />
+          <button
+            className="w-full py-3 px-4 leading-none text-white bg-indigo-500 hover:bg-indigo-600 rounded-lg tracking-wide"
+            type="submit"
+          >
+            Add Ticket
+          </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default AddTicket;
--- components/TicketsList.jsx
+++ components/TicketsList.jsx
@@ -3,26 +3,29 @@ import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-
 export async function getTickets() {
   try {
-    const res = await fetch("http://localhost:3000/api/tickets", {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      "http://localhost:3000/api/tickets" ||
+        "https://helpdesk-crud.onrender.com/api/tickets",
+      {
+        cache: "no-store",
+      }
+    );
     if (!res.ok) {
       throw new Error(`Failed to fetch tickets: ${res.status}`);
     }
     const tickets = await res.json();
-    console.log( tickets);
+    console.log(tickets);
     return tickets;
   } catch (error) {
     console.error("Error Loading Tickets", error);
     throw error;
   }
-};
+}
 
-const TicketsList = async() => {
-  const tickets  = await getTickets();
+const TicketsList = async () => {
+  const tickets = await getTickets();
 
   if (!tickets || tickets.length === 0) {
     return (
@@ -34,37 +37,34 @@ const TicketsList = async() => {
 
   return (
     <>
-    {tickets.map((ticket) => (
-      <div  key={ticket._id}>
-        <div className="p-4 border my-3 mx-3 flex justify-between gap-5 items-start rounded-lg bg-orange-200">
-          <div className=" ">
-            <h2 className="font-bold ">{ticket.title}</h2>
-            <p>{ticket.description}</p>
-            <p className="text-sm text-gray-400">
-              Created: 
-              {new Date(ticket.createdAt).toLocaleDateString("en-US")}
-            </p>
-            <p className="text-sm text-gray-400">
-              Updated:
-              {new Date(ticket.updatedAt).toLocaleDateString("en-US")}
-            </p>
-          </div>
+      {tickets.map((ticket) => (
+        <div key={ticket._id}>
+          <div className="p-4 border my-3 mx-3 flex justify-between gap-5 items-start rounded-lg bg-orange-200">
+            <div className=" ">
+              <h2 className="font-bold ">{ticket.title}</h2>
+              <p>{ticket.description}</p>
+              <p className="text-sm text-gray-400">
+                Created:
+                {new Date(ticket.createdAt).toLocaleDateString("en-US")}
+              </p>
+              <p className="text-sm text-gray-400">
+                Updated:
+                {new Date(ticket.updatedAt).toLocaleDateString("en-US")}
+              </p>
+            </div>
 
-          <div className="flex gap-2 ">
-            <RemoveBtn />
-         
+            <div className="flex gap-2 ">
+              <RemoveBtn />
 
-            <Link href={`/editTickets/${ticket._id}`} as="">
-              <HiPencilAlt size={24} />
-            </Link>
+              <Link href={`/editTickets/${ticket._id}`} as="">
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    )  )}
+      ))}
     </>
   );
-
-  
 };
 
 export default TicketsList;
