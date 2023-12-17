66ae8a5cabcd71cb4e5833e3cf6876975d201399
shoaib <shoaibhossain24@gmail.com>
2023-12-18 01:34:14 +0600

    Delete unused code and update ticket data in TicketsList component

--- components/TicketsList.jsx
+++ components/TicketsList.jsx
@@ -4,29 +4,51 @@ import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
 
-export async function getTickets() {
-  try {
-    const res = await fetch(
-      "http://localhost:3000/api/tickets" ||
-        "https://helpdesk-crud.onrender.com/api/tickets",
-      {
-        cache: "no-store",
-      }
-    );
-    if (!res.ok) {
-      throw new Error(`Failed to fetch tickets: ${res.status}`);
-    }
-    const tickets = await res.json();
+// export async function getTickets() {
+//   try {
+//     const res = await fetch(
+//       "http://localhost:3000/api/tickets" ||
+//         "https://helpdesk-crud.onrender.com/api/tickets",
+//       {
+//         cache: "no-store",
+//       }
+//     );
+//     if (!res.ok) {
+//       throw new Error(`Failed to fetch tickets: ${res.status}`);
+//     }
+//     const tickets = await res.json();
    
-    return tickets;
-  } catch (error) {
-    console.error("Error Loading Tickets", error);
-    throw error;
-  }
-}
+//     return tickets;
+//   } catch (error) {
+//     console.error("Error Loading Tickets", error);
+//     throw error;
+//   }
+// }
 
 const TicketsList = async () => {
-  const tickets = await getTickets();
+  //const tickets = await getTickets();
+
+  const tickets = [
+   
+    {
+      "title": "Database Indexing Error",
+      "description": "Database indexing error leading to slow query performance. Reindex the database to improve query execution.",
+      "createdAt": "2023-10-18T18:10:00.000+00:00",
+      "updatedAt": "2023-10-19T19:30:00.000+00:00"
+    },
+    {
+      "title": "Error Handling Framework Update",
+      "description": "Update error handling framework to address application exceptions more efficiently. Enhance error logging and reporting.",
+      "createdAt": "2023-10-19T20:00:00.000+00:00",
+      "updatedAt": "2023-10-20T21:25:00.000+00:00"
+    },
+    {
+      "title": "External API Integration Failure",
+      "description": "Failure observed in the integration with external APIs. Debug and fix the integration issues for seamless data exchange.",
+      "createdAt": "2023-10-20T22:00:00.000+00:00",
+      "updatedAt": "2023-10-21T23:20:00.000+00:00"
+    }
+  ]
 
   if (!tickets || tickets.length === 0) {
     return (
---
+++ rand/page.jsx
@@ -0,0 +1,11 @@
+import TopicsList from "@/components/TicketsList";
+
+
+export default function Home() {
+  return (
+    <main className="flex max-h-screen flex-col ">
+    <TopicsList />
+   
+    </main>
+  )
+}
