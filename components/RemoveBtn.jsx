10fb27dffb07e4981dffa5b7183032c110f8bff3
shoaib <shoaibhossain24@gmail.com>
2023-12-17 09:18:09 +0600

    Refactor RemoveBtn component to include a confirmation modal

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,26 +1,53 @@
-"use client";
-
-import { HiOutlineTrash } from "react-icons/hi";
+'use client';
+import { useState } from 'react';
+import { HiOutlineTrash, HiOutlineCheck, HiOutlineX } from 'react-icons/hi';
 
 const RemoveBtn = ({ id }) => {
-  console.log("id:" + id);
+  const [showModal, setShowModal] = useState(false);
 
   const deleteTicket = async () => {
-    const confirmDelete = confirm(
-      "Are you sure you want to delete this ticket?"
-    );
-    if (confirmDelete) {
-      await fetch(`http://localhost:3000/api/tickets?id=${id}`, {
-        method: "DELETE",
-      });
-     window.location.reload();  
-    }
+    await fetch(`http://localhost:3000/api/tickets?id=${id}`, {
+      method: 'DELETE',
+    });
+    window.location.reload();
   };
 
   return (
-    <button onClick={deleteTicket}>
-      <HiOutlineTrash size={24} color="red" />
-    </button>
+    <>
+      <button onClick={() => setShowModal(true)}>
+        <HiOutlineTrash size={24} color='red' />
+      </button>
+
+      {showModal && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={() => setShowModal(false)}
+        >
+          <div
+            className="bg-white p-6 rounded-lg shadow-xl"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <p className="mb-4 text-lg font-semibold">Are you sure you want to delete this ticket?</p>
+            <div className="flex gap-4">
+              <button
+                className="flex items-center gap-2 px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600"
+                onClick={deleteTicket}
+              >
+                <HiOutlineCheck />
+                Yes
+              </button>
+              <button
+                className="flex items-center gap-2 px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600"
+                onClick={() => setShowModal(false)}
+              >
+                <HiOutlineX />
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
